7ebfca44d698b4fcdd06a808da95687352d95163
viktor <devel.andro.2016@gmail.com>
2021-08-30 13:30:15 +0200

    Manufacturers List view

---
+++ BitnetVehicle/Manufacturer.swift
@@ -0,0 +1,21 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by Borbély Viktor on 2021. 08. 28..
+//
+
+import Foundation
+
+struct Manufacturer {
+    var name: String
+    var vehicleTypes: [String]
+}
+
+extension Manufacturer {
+    static var testData = [
+        Manufacturer(name: "Manufacturer #1", vehicleTypes: ["Type #1", "Type #2","Type #1", "Type #2","Type #1", "Type #2","Type #1", "Type #2","Type #1", "Type #2"]),
+        Manufacturer(name: "Manufacturer #2", vehicleTypes: ["Type #1", "Type #2"]),
+    ]
+}
+
---
+++ BitnetVehicle/ManufacturerListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ManufacturerListCell.swift
+//  Today
+//
+//  Created by Borbély Viktor on 2021. 08. 28..
+//
+
+import UIKit
+
+class ManufacturerListCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var typesLabel: UILabel!
+    
+}
